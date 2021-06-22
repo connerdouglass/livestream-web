1eab660e02cb70395ac420e31921b9f7a884de95
connerdouglass <conner.douglass@gmail.com>
2021-06-22 10:49:02 -0700

    Updated base URLs for first deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	api_baseurl: 'https://live-api.alialexander.com',
-	hls_baseurl: 'https://live-play.alialexander.com',
+	api_baseurl: 'https://live-api.alialexander.org',
+	hls_baseurl: 'https://live-play.alialexander.org',
 };
