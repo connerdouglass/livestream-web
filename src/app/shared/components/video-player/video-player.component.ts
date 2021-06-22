9f2a4a14c6528f86e3bf606dffb59c6d2815c1a1
connerdouglass <conner.douglass@gmail.com>
2021-06-22 12:47:25 -0700

    Playback indicator fix

--- src/app/shared/components/video-player/video-player.component.ts
+++ src/app/shared/components/video-player/video-player.component.ts
@@ -37,17 +37,22 @@ export class VideoPlayer {
     @ViewChild('video') public set v(video_element: ElementRef<HTMLVideoElement> | undefined) {
         this.video_element = video_element?.nativeElement;
         const video_ready_handler = () => {
-            this.video_ready = true;
             this.video_element!.muted = true;
-            this.video_element?.play();
+            this.video_element!.play();
         };
         if (this.video_element) {
             if (Hls.isSupported()) {
                 this.hls.attachMedia(this.video_element);
                 this.hls.on(Hls.Events.MEDIA_ATTACHED, video_ready_handler);
+                this.hls.on(Hls.Events.MANIFEST_LOADED, () => {
+                    this.video_ready = true;
+                });
             } else {
                 this.video_element.src = this.source_url;
-                this.video_element.addEventListener('canplay', video_ready_handler);
+                this.video_element.addEventListener('canplay', () => {
+                    this.video_ready = true;
+                    video_ready_handler();
+                });
             }
         }
     }
